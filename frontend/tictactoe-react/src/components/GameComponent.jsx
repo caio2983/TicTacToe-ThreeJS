4ff47b21bfdcfd19a330817e5e6aed2da04d4952
caio2983 <caioluna2002@gmail.com>
2025-06-10 12:59:46 -0300

    fix: fix import

--- frontend/tictactoe-react/src/components/GameComponent.jsx
+++ frontend/tictactoe-react/src/components/GameComponent.jsx
@@ -11,7 +11,7 @@ import OPiece from "./pieces/OPiece";
 import XPiece from "./pieces/XPiece";
 import { InfiniteGridHelper } from "./grid";
 import NeonRedGrid3x3 from "./NeonRedGrid3x3";
-import PlayerList from "./PlayerList";
+import PlayerList from "./playerList/PlayerList";
 import WaitAlert from "./alerts/WaitAlert";
 
 export default function GameComponent() {
